67affaf2d94c7d94251da7d0767ab687868a96f7
Valentin <valentin.podda@etu.unice.fr>
2023-10-24 14:45:01 +0200

    validate order

--- src/main/java/fr/unice/polytech/biblio/OrderController.java
+++ src/main/java/fr/unice/polytech/biblio/OrderController.java
@@ -47,4 +47,8 @@ public class OrderController {
             return false;
         }
     }
+
+    public void validateOrder(Order order) {
+        order.setOrderState(OrderState.VALIDATED);
+    }
 }
